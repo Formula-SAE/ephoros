d855368ddd5149893302452dce0052355a4f22d5
Antonio Lentini <111151429+lentscode@users.noreply.github.com>
2025-07-22 21:32:26 +0200

    test: add unit tests for DB

---
+++ server/internal/db/test_utils.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func testDB() (*gorm.DB, func(), error) {
+	baseDbUrl := os.Getenv("BASE_DB_URL")
+	dbUrl := os.Getenv("DB_URL")
+
+	dbName := fmt.Sprintf("test_db_%d", rand.Int())
+	rootDB, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if err := rootDB.Exec("CREATE DATABASE " + dbName).Error; err != nil {
+		return nil, nil, err
+	}
+
+	dsnTest := baseDbUrl + dbName
+	testDB, err := gorm.Open(postgres.Open(dsnTest), &gorm.Config{})
+	if err != nil {
+		rootDB.Exec("DROP DATABASE " + dbName)
+		return nil, nil, err
+	}
+
+	testDB.AutoMigrate(&Section{}, &Module{}, &Sensor{}, &Record{})
+
+	cleanup := func() {
+		sqlDB, _ := testDB.DB()
+		sqlDB.Close()
+		rootDB.Exec("DROP DATABASE " + dbName)
+		sqlRoot, _ := rootDB.DB()
+		sqlRoot.Close()
+	}
+
+	return testDB, cleanup, nil
+}
