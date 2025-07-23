063860ac2b532b60411c7e570e3161d779a62e4f
Antonio Lentini <111151429+lentscode@users.noreply.github.com>
2025-07-23 23:14:52 +0200

    feat: add API routes
    Also fix tests because of struct changes.

--- server/internal/api/api.go
+++ server/internal/api/api.go
@@ -3,20 +3,97 @@ package api
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/json"
 	"errors"
+	"net/http"
 
 	"github.com/ApexCorse/ephoros/server/internal/db"
+	"github.com/gorilla/mux"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type API struct {
-	db *db.DB
+	db      *db.DB
+	r       *mux.Router
+	address string
 }
 
-func NewAPI(db *db.DB) *API {
+func NewAPI(address string, db *db.DB, r *mux.Router) *API {
 	return &API{
-		db: db,
+		db:      db,
+		r:       r,
+		address: address,
 	}
+}
+
+func (a *API) Start() {
+	a.r.HandleFunc("/auth", a.handleAuth).Methods("POST")
+	a.r.HandleFunc("/data", a.handleSendData).Methods("POST")
+
+	http.ListenAndServe(a.address, a.r)
+}
+
+func (a *API) handleAuth(w http.ResponseWriter, r *http.Request) {
+	username := r.Header.Get("Username")
+	password := r.Header.Get("Password")
+
+	if err := a.validateUser(username, password); err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(
+		map[string]string{
+			"message": "Authorized",
+		},
+	)
+}
+
+func (a *API) handleSendData(w http.ResponseWriter, r *http.Request) {
+	username := r.Header.Get("Username")
+	password := r.Header.Get("Password")
+
+	if err := a.validateUser(username, password); err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	body := &DataRequestBody{}
+	if err := json.NewDecoder(r.Body).Decode(body); err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	if !body.Validate() {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	sensor, err := a.db.GetSensorByNameAndModuleAndSection(
+		body.Sensor,
+		body.Module,
+		body.Section,
+		body.From,
+		body.To,
+	)
+	if err != nil {
+		http.Error(w, "sensor not found", http.StatusNotFound)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(
+		map[string]any{
+			"module":  body.Module,
+			"name":    sensor.Name,
+			"records": sensor.Records,
+			"section": body.Section,
+		},
+	)
 }
 
 func (a *API) validateUser(username, password string) error {
