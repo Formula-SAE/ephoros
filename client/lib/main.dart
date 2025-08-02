fc5d429c030f054b5e41a64e6d48ef7a62290963
Antonio Lentini <111151429+lentscode@users.noreply.github.com>
2025-08-02 15:33:59 +0200

    style: format code

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() {
   runApp(const MainApp());
@@ -8,13 +8,11 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context) => const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text("Hello World!"),
         ),
       ),
-    );
-  }
+  );
 }
